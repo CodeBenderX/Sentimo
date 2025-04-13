3825607f2df190622e4b3f278338e8410f634192
lmenil <lmenil@my.centennialcollege.ca>
2025-04-13 14:57:20 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next'
+//import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'sentimo',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+// export const metadata: Metadata = {
+//   title: 'sentimo',
+//   description: 'Created with v0',
+//   generator: 'v0.dev',
+// }
 
 export default function RootLayout({
   children,
